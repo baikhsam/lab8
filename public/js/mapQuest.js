0dd68172210e2d7057b2a8ddb3a11a6f9e723062
Sam Baik <baikhsam@gmail.com>
2020-02-27 12:41:07 -0800

    finished lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,15 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'VwqVFKWZ0ENerTy4QNgBeVyZ84LrlI1U';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+	  center: [32.8788033, -117.238106],
+	  layers: L.mapquest.tileLayer('map'),
+	  zoom: 12,
+	  zoomControl: false
+	});
+
+	//Add marker to map
+	L.marker([32.8788033, -117.238106]).addTo(map);
 }
